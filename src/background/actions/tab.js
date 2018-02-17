0154cdc74a8f735c5f6b1e8dcc7a8adfaa0aade8
pip2150 <dhrcogns2002@hanmail.net>
2018-02-18 03:18:47 +0900

    support http referer

--- src/background/actions/tab.js
+++ src/background/actions/tab.js
@@ -1,8 +1,29 @@
-const openNewTab = (url) => {
+function onBeforeSendHeaders(url) {
+  browser.webRequest.onBeforeSendHeaders.addListener(
+    (request) => {
+      let referer = null;
+      for (let header of request.requestHeaders) {
+        if (header.name.toLowerCase() === "referer" && header.value) {
+          referer = header;
+          break;
+        }
+      }
+      referer.value = url;
+
+      return { requestHeaders: request.requestHeaders };
+    },
+    { urls: ["<all_urls>"] },
+    ["blocking", "requestHeaders"]
+  );
+}
+
+const openNewTab = (url, tab) => {
+  onBeforeSendHeaders(tab.url);
   return browser.tabs.create({ url: url });
 };
 
 const openToTab = (url, tab) => {
+  onBeforeSendHeaders(tab.url);
   return browser.tabs.update(tab.id, { url: url });
 };
 
